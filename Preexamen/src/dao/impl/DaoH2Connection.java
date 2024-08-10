48a23a70eb8c370a090197ec424b2d3709e4065b
Kbri17 <104737155+Kbri17@users.noreply.github.com>
2024-08-09 23:00:01 -0500

    primer commit

---
+++ Preexamen/src/dao/impl/DaoH2Connection.java
@@ -0,0 +1,19 @@
+package dao.impl;
+
+import dao.IDao;
+import model.Veterinario;
+
+import java.util.List;
+
+public class DaoH2Connection implements IDao<Veterinario> {
+
+    @Override
+    public Veterinario guardar(Veterinario veterinario) {
+        return null;
+    }
+
+    @Override
+    public List<Veterinario> listarTodos() {
+        return null;
+    }
+}
---
+++ Preexamen/src/service/VeterinarioService.java
@@ -0,0 +1,7 @@
+package service;
+
+public class VeterinarioService {
+    public static void main(String[] args) {
+        System.out.println("Holamundo");
+    }
+}
